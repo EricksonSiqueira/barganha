57fb1136b37e7dd370280297a5e120c71fa1c062
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-21 18:17:22 -0300

    feat: add links to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,30 @@ const kalam = Kalam({ subsets: ['devanagari'], weight: ['400', '700'] });
 
 import styles from './styles.module.css';
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
         <h2 className={`${kalam.className} ${styles.headerTitle}`}>Barganha</h2>
+        <nav>
+          <ul className={styles.headerNav}>
+            <li>
+              <a href="https://github.com/EricksonSiqueira" target="_blank">
+                <FaGithub size={28} color="E4E4E7" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ericksonsiqueira/"
+                target="_blank"
+              >
+                <FaLinkedin size={28} color="E4E4E7" />
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
