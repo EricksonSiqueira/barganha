34f1f1871edea17dc0febfda9cd071cb1b262cb5
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-25 13:38:53 -0300

    feat: create empty state

--- src/components/BeerList/index.tsx
+++ src/components/BeerList/index.tsx
@@ -1,6 +1,8 @@
 import { Beer } from '@/types/beer';
 import styles from './styles.module.css';
 import BeerCard from './BeerCard';
+import { PiBeerBottleFill } from 'react-icons/pi';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 export interface BeerListProps {
   beers: Beer[];
@@ -14,14 +16,31 @@ export default function BeerList({
 }: BeerListProps) {
   return (
     <div className={styles.beerListWrapper}>
-      {beers.map((beer) => (
-        <BeerCard
-          beer={beer}
-          cheapestBeerId={cheapestBeer?.id}
-          key={beer.id}
-          removeBeer={removeBeer}
-        />
-      ))}
+      {beers.length > 0 ? (
+        beers.map((beer) => (
+          <BeerCard
+            beer={beer}
+            cheapestBeerId={cheapestBeer?.id}
+            key={beer.id}
+            removeBeer={removeBeer}
+          />
+        ))
+      ) : (
+        <div className={styles.beersNotFound}>
+          <div className={styles.beersNotFoundIconsWrapper}>
+            <PiBeerBottleFill size={128} color="#eab308" />
+            <AiOutlineCloseCircle
+              size={32}
+              color="#EF4444"
+              className={styles.beersNotFoundCloseIcon}
+            />
+          </div>
+          <div className={styles.beersNotFoundTextWrapper}>
+            <h2>Nenhuma cerveja encontrada</h2>
+            <p>Adicione cervejas e comece a economizar!</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
